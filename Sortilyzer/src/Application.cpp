f46ed82fff5f8485ac2abaaff0408e512d1e05b7
ayaankhan98 <ayaankhan98@gmail.com>
2021-03-01 23:10:45 +0530

    applied LLVM format

--- Sortilyzer/src/Application.cpp
+++ Sortilyzer/src/Application.cpp
@@ -1,61 +1,63 @@
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+#include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <string>
 
-#include <imgui.h>
-#include <imgui-SFML.h>
-#include <SFML/Graphics.hpp>
-
-#include "Test/Test.hpp"
 #include "BubbleSortTest.hpp"
 #include "Core.hpp"
+#include "Test/Test.hpp"
 
 int main() {
-	srand(time(0));
+  srand(time(0));
 
-	sf::RenderWindow window(sf::VideoMode(sortilyzer::_WIDTH, sortilyzer::_HEIGHT), sortilyzer::windowTitle);
+  sf::RenderWindow window(
+      sf::VideoMode(sortilyzer::_WIDTH, sortilyzer::_HEIGHT),
+      sortilyzer::windowTitle);
 
-	window.setActive(true);
-	window.setFramerateLimit(sortilyzer::_FRAME_RATE);
+  window.setActive(true);
+  window.setFramerateLimit(sortilyzer::_FRAME_RATE);
 
-	bool isOpenWindow = true;
-	ImGui::SFML::Init(window);
-	sf::Clock deltaClock;
+  bool isOpenWindow = true;
+  ImGui::SFML::Init(window);
+  sf::Clock deltaClock;
 
-	sortilyzer::Test* currentTest(nullptr);
-	sortilyzer::TestMenu* testMenu = new sortilyzer::TestMenu(currentTest);
-	currentTest = testMenu;
+  sortilyzer::Test *currentTest(nullptr);
+  sortilyzer::TestMenu *testMenu = new sortilyzer::TestMenu(currentTest);
+  currentTest = testMenu;
 
-	testMenu->RegisterTest<sortilyzer::test::BubbleSortTest>("Bubble Sort", window);
+  testMenu->RegisterTest<sortilyzer::test::BubbleSortTest>("Bubble Sort",
+                                                           window);
 
-	while (isOpenWindow) {
-		sf::Event e;
-		while (window.pollEvent(e)) {
-			ImGui::SFML::ProcessEvent(e);
-			if (e.type == sf::Event::Closed) {
-				isOpenWindow = false;
-			}
-		}
+  while (isOpenWindow) {
+    sf::Event e;
+    while (window.pollEvent(e)) {
+      ImGui::SFML::ProcessEvent(e);
+      if (e.type == sf::Event::Closed) {
+        isOpenWindow = false;
+      }
+    }
 
-		ImGui::SFML::Update(window, deltaClock.restart());
+    ImGui::SFML::Update(window, deltaClock.restart());
 
-		window.clear();
-		if (currentTest) {
-			currentTest->OnRender();
-			currentTest->OnUpdate();
-			ImGui::Begin("Test Menu");
+    window.clear();
+    if (currentTest) {
+      currentTest->OnRender();
+      currentTest->OnUpdate();
+      ImGui::Begin("Test Menu");
 
-			if (currentTest != testMenu && ImGui::Button("<-")) {
-				delete currentTest;
-				currentTest = testMenu;
-			}
-			currentTest->OnImGuiRender();
+      if (currentTest != testMenu && ImGui::Button("<-")) {
+        delete currentTest;
+        currentTest = testMenu;
+      }
+      currentTest->OnImGuiRender();
 
-			ImGui::End();
-		}
+      ImGui::End();
+    }
 
-
-		ImGui::SFML::Render(window);
-		window.display();
-	}
-	ImGui::SFML::Shutdown();
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+  ImGui::SFML::Shutdown();
 }
--- Sortilyzer/src/BubbleSortTest.hpp
+++ Sortilyzer/src/BubbleSortTest.hpp
@@ -2,20 +2,18 @@
 #include "Test/Test.hpp"
 
 namespace sortilyzer {
-	namespace test {
-		class BubbleSortTest : public Test {
-		public:
-			BubbleSortTest(sf::RenderWindow*);
-			~BubbleSortTest() {
-				std::cout << "Destroyed Bubble Sort Test\n";
-			}
-			void OnUpdate() override;
-			void OnRender() override;
-			void OnImGuiRender() override;
+namespace test {
+class BubbleSortTest : public Test {
+public:
+  BubbleSortTest(sf::RenderWindow *);
+  ~BubbleSortTest() { std::cout << "Destroyed Bubble Sort Test\n"; }
+  void OnUpdate() override;
+  void OnRender() override;
+  void OnImGuiRender() override;
 
-		private:
-			sf::RenderWindow* m_Window;
-			float m_StripColor[4] = {0.0, 0.9, 0.0, 1.0};
-		};
-	}
-}
+private:
+  sf::RenderWindow *m_Window;
+  float m_StripColor[4] = {0.0, 0.9, 0.0, 1.0};
+};
+} // namespace test
+} // namespace sortilyzer
