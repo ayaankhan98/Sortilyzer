606c728fec7e47749a14cdc0abe0f6cd3b1e7bcd
ayaankhan98 <ayaankhan98@gmail.com>
2021-03-13 23:12:44 +0530

    Added QuickSort Test

--- Sortilyzer/src/Core.hpp
+++ Sortilyzer/src/Core.hpp
@@ -6,15 +6,13 @@ namespace sortilyzer {
 constexpr int _WIDTH = 1640;
 constexpr int _HEIGHT = 768;
 
-constexpr int SIZE = 50;
+constexpr int SIZE = 100;
 
 constexpr float _XPOS = 20.0f;
-constexpr int _STRIP_WIDTH = 20;
+constexpr int _STRIP_WIDTH = 10;
 constexpr int _Y_DOWN_OFFSET = 200;
 constexpr int _MIN_STRIP_HEIGHT = 20;
-constexpr int _MAX_STRIP_HEIGHT = 300;
-
-constexpr int _FRAME_RATE = 10;
+constexpr int _MAX_STRIP_HEIGHT = 400;
 
 const std::string windowTitle = "My Application Window";
 } // namespace sortilyzer
---
+++ Sortilyzer/src/QuickSortTest.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Test/Test.hpp"
+#include <stack>
+
+namespace sortilyzer {
+namespace test {
+class QuickSortTest : public Test {
+public:
+  QuickSortTest(sf::RenderWindow *);
+  ~QuickSortTest() { std::cout << "Destroying QuickSort Test\n"; }
+
+  void OnRender() override;
+  void OnUpdate() override;
+  void OnImGuiRender() override;
+
+private:
+  int getPivotAsMiddle(int start, int end);
+  void quick();
+  sf::RenderWindow *m_Window;
+  float m_StripColor[4] = {0.0, 0.9, 0.0, 1.0};
+  std::stack<int> lower, upper;
+  int N, loc, beg, end, left, right;
+  bool state = false;
+};
+} // namespace test
+} // namespace sortilyzer
